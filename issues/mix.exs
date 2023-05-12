de0591c0dc6d4f6e26b1c27bf690846342f0fef3
Castic Jehin <jehincastic@gmail.com>
2023-05-13 01:19:20 +0530

    Bumped Elixir Version

--- issues/mix.exs
+++ issues/mix.exs
@@ -6,7 +6,7 @@ defmodule Issues.MixProject do
       app: :issues,
       escript: escript_config(),
       version: "0.1.0",
-      elixir: "~> 1.13",
+      elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
